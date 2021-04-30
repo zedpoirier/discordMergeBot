89da1bf2faec86050eb081efb2351e3e991a57c2
Zed Poirier <49734422+zedpoirier@users.noreply.github.com>
2021-04-30 10:43:16 -0400

    Update discordMerge.py

--- discordMerge.py
+++ discordMerge.py
@@ -1,15 +1,18 @@
 #https://support.discord.com/hc/en-us/community/posts/360032701912-Channel-Merging
+# https://discordpy.readthedocs.io/en/latest/api.html#discord.User.display_name
 
 import os
+import asyncio
 import discord
 from dotenv import load_dotenv
+import discord.ext.commands
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv("DISCORD_GUILD")
-client = discord.Client()
-
 testID = 837354270939545670
+
+client = discord.ext.commands.Bot(command_prefix='!')
 
 @client.event
 async def on_ready():
@@ -17,24 +20,31 @@ async def on_ready():
     guild = client.guilds[0]
     print(f'{guild.name} (id: {guild.id})')
 
-@client.event
-async def on_message(message):
-        # don't respond to ourselves
-        if message.content == 'ping':
-            await message.channel.send('pong')
-        if message.content == 'pong':
-            await message.channel.send('ping')
+@client.command(name='clear', help='this command will clear msgs')
+async def clear(ctx, amount = 5):
+    await ctx.channel.purge(limit=amount)
+
+@client.command(name='history', help='save history of channel')
+async def history(ctx, amount = 5):
+    msgs = {}
+    counter = 0
+    async for message in ctx.channel.history(limit=amount):
+        counter += 1
+        auth = str(message.author.display_name) + "-" + str(counter)
+        msgs[auth] = message.content
+    print(msgs)
+
+@client.command(name='nick', help='change bot name')
+async def nick(ctx, newname = "noname"):
+    await ctx.guild.get_member(client.user.id).edit(nick=newname)
+
+@client.command(name='pong', help='test')
+async def pong(ctx):
+    await ctx.send("pong")
+
+
 
 client.run(TOKEN)
-
-
-
-
-
-
-
-
-
 
 
 # connect to guild
